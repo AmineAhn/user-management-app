ebe65ea1e09c081aa01d96b658d9c0bbf6c40d16
AHNICHE Amine <amineahniche@gmail.com>
2025-10-21 18:18:20 +0200

    fix names

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,18 +1,18 @@
 import { FastifyInstance } from "fastify";
 import { loginSchema } from "../schemas/auth.schema";
-import { userSchema } from "../schemas/user.schema";
 import { UserService } from "../services/user.service";
 import bcrypt from "bcrypt";
+import { createUserSchema } from "../schemas/user.schema";
 
 export async function authRoutes(app: FastifyInstance) {
   app.post("/register", async (req, reply) => {
-    const parsed = userSchema.safeParse(req.body);
+    const parsed = createUserSchema.safeParse(req.body);
     if (!parsed.success) return reply.status(400).send(parsed.error);
 
-    const existing = await UserService.findByEmail(parsed.data.email);
+    const existing = await UserService.getUserByEmail(parsed.data.email);
     if (existing) return reply.status(400).send({ message: "Email already exists" });
 
-    const user = await UserService.create(parsed.data);
+    const user = await UserService.createUser(parsed.data);
     reply.send(user);
   });
 
@@ -20,7 +20,7 @@ export async function authRoutes(app: FastifyInstance) {
     const parsed = loginSchema.safeParse(req.body);
     if (!parsed.success) return reply.status(400).send(parsed.error);
 
-    const user = await UserService.findByEmail(parsed.data.email);
+    const user = await UserService.getUserByEmail(parsed.data.email);
     if (!user) return reply.status(401).send({ message: "Invalid credentials" });
 
     const match = await bcrypt.compare(parsed.data.password, user.password);
